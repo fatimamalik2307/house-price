02b68a4a78ce14280ef5292023419c8e94478d4e
fatimamalik2307 <fatimamalikk188@gmail.com>
2025-10-17 18:15:42 +0500

    Fix syntax errors in main branch

--- housepk_app.py
+++ housepk_app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return \"Home Page\"
+    return "Home Page"
 
-if __name__ == \"__main__\":
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
