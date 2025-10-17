3b0127df3cc0f628a59b434082fabb228e336411
fatimamalik2307 <fatimamalikk188@gmail.com>
2025-10-17 18:08:34 +0500

    Alice: Simple dashboard feature

--- housepk_app.py
+++ housepk_app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return \"Home Page\"
+    return "Home Page for alice"
+@app.route('/about')
+def about():
+    return "About Page for alice"
 
-if __name__ == \"__main__\":
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
