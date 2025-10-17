193cbc995748552dd11d5c7a09c1add31f5d3194
fatimamalik2307 <fatimamalikk188@gmail.com>
2025-10-17 17:51:26 +0500

    Add clean housepk_app.py for Git exercise

---
+++ housepk_app.py
@@ -0,0 +1,15 @@
+﻿# housepk_app.py - Fresh Start for Git Exercise
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return \"🏠 Home Page - Clean Start\"
+
+@app.route('/about')
+def about():
+    return \"📖 About Page\"
+
+if __name__ == \"__main__\":
+    app.run(debug=True, port=5000)
